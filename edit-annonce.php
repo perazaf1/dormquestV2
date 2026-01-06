cb70acd3257d40fab21fec1fc2006f6d7b8e0a77
perazaf1 <pe.razafindrakoto@gmail.com>
2026-01-06 23:11:31 +0100

    ajout du début de la page modification des annonces

---
+++ edit-annonce.php
@@ -0,0 +1,32 @@
+<?php
+// favoris.php - Mes favoris (�tudiant)
+session_start();
+
+require_once __DIR__ . '/includes/db.php';
+require_once 'includes/auth.php';
+
+
+require_loueur()
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Modifier une annonce - DormQuest</title>
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/edit-annonce.css">
+    <style>
+        
+    </style>
+</head>
+
+<body>
+    <?php include 'includes/header.php'; ?>
+
+    <?php include 'includes/footer.php'; ?>
+</body>
